cc10423838a1e5e43f5f028043c04f1b1b8330c8
明 <2353190325@qq.com>
2023-11-28 20:36:16 +0800

    导入excel表格，查找姓名列

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,11 +2,11 @@
   <div class="container">
     <div>
       <input type="file" ref="fileInput" @change="handleFileChange" accept=".xls,.xlsx" />
-    <button @click="processExcel">处理Excel文件</button>
+      <button @click="processExcel">处理Excel文件</button>
     </div>
-    <button class="btn" type="button" @click="startLottery" >随机点名</button>
+    <button class="btn" type="button" @click="startLottery">随机点名</button>
     <div class="grid">
-      <div v-for="(item, index) in list" :key="index" class="grid-item"  :style="{ backgroundColor: item.bgcolor}" >
+      <div v-for="(item, index) in list" :key="index" class="grid-item" :style="{ backgroundColor: item.bgcolor }">
         {{ item.name }}
       </div>
     </div>
@@ -20,20 +20,47 @@ const list = ref([])
 const state = reactive({
   lotteryCount: 0,
 })
-
 onMounted(() => {
-  // for (let i = 0; i < 45; i++) {
-  //   list.value.push({ name: "学生" + i, bgcolor: 'white' })
-  // }
+
 })
+const isStatus=1
+const  noStatus=-1
 
 const selectedFile = ref(null);
 const workbook = ref(null);
-
 const handleFileChange = (event) => {
   selectedFile.value = event.target.files[0];
 };
-
+//查询姓名字段是否存在
+const isName = (jsonData) => {
+  let nameColumnIndex = -1;
+  let nameRowIndex = -1;
+  // 遍历表格，寻找包含"姓名"关键词的单元格
+  for (let r = 0; r < jsonData.length; r++) {
+    const row = jsonData[r];
+    for (let c = 0; c < row.length; c++) {
+      if (row[c] === '姓名') {
+        nameColumnIndex = c;
+        nameRowIndex = r;
+        console.log("找到");
+        break;
+      }
+    }
+    if (nameColumnIndex !== -1) {
+      break; // 找到后跳出循环
+    }
+  }
+  const data={}
+  if (nameColumnIndex !== -1 && nameRowIndex !== -1) {
+     data.msg = 1,
+     data.colIndex=nameColumnIndex,
+     data.rowIndex=nameRowIndex
+  } else {
+    data.msg=-1
+  }
+  return data
+}
+//读取excel文件，查找姓名列输入
 const processExcel = () => {
   if (selectedFile.value) {
     const reader = new FileReader();
@@ -44,11 +71,18 @@ const processExcel = () => {
       const sheetName = workbook.value.SheetNames[0]; // 获取第一个工作表名称
       const worksheet = workbook.value.Sheets[sheetName];   //获取表数据
       const jsonData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-      const nameColumnIndex = jsonData[0].findIndex((col) => col === '姓名'); // 找到姓名列的索引
-      const names = jsonData.slice(1).map((row) => row[nameColumnIndex]); // 获取姓名列的数据
-      names.forEach(item=>{
+      const res =isName(jsonData)
+      console.log(res);
+      if (res.msg ==isStatus) {
+        const names = jsonData.slice(res.rowIndex + 1).map((row) => row[res.colIndex]); // 从姓名所在行之后获取姓名列的数据
+        names.forEach(item => {
         list.value.push({ name: item, bgcolor: 'white' })
       })
+      } else if(res.msg ==noStatus){
+        alert("找不到姓名，曼莉姐，检查一下表格") 
+      }else{
+        alert("报错了，请联系我")
+      }
     };
     reader.readAsArrayBuffer(selectedFile.value);
   }
@@ -58,24 +92,23 @@ const colors = ['red', 'blue', 'green', 'yellow', 'purple', 'orange', 'pink'];
 
 // 抽奖
 const startLottery = () => {
-  console.log(12);
-    state.lotteryCount = 0
-    const randomUser = setInterval(() => {
-      const activeIndex = Math.floor(Math.random() * list.value.length)
-      const randomColor = colors[Math.floor(Math.random() * colors.length)];// 随机生成颜色
-      list.value[activeIndex].bgcolor = randomColor
-      list.value.forEach((item, index) => {
-        if (activeIndex != index) {
-          item.bgcolor = "white"
+  state.lotteryCount = 0
+  const randomUser = setInterval(() => {
+    const activeIndex = Math.floor(Math.random() * list.value.length)
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];// 随机生成颜色
+    list.value[activeIndex].bgcolor = randomColor
+    list.value.forEach((item, index) => {
+      if (activeIndex != index) {
+        item.bgcolor = "white"
       }
-      })
+    })
 
-      state.lotteryCount++
-      if (state.lotteryCount >= 10) {
-        clearInterval(randomUser)
-      }
-    }, 150) // 每秒运行一次抽奖
-  }
+    state.lotteryCount++
+    if (state.lotteryCount >= 10) {
+      clearInterval(randomUser)
+    }
+  }, 150) // 每秒运行一次抽奖
+}
 </script>
 
 <style scoped>
@@ -104,12 +137,13 @@ const startLottery = () => {
   border: 1px solid #000;
   background-color: white;
 }
-.btn{
+
+.btn {
   border: none;
   padding: 0px 20px;
   height: 40px;
   border-radius: 5%;
-  background-color:#409eff;
+  background-color: #409eff;
   color: #ffffff;
 }
 </style>
