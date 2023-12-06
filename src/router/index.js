2b33a7a2e64eb8d7d36bffc994eb68369d5c116e
明 <2353190325@qq.com>
2023-12-06 15:14:34 +0800

    依赖分包打包

--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory,createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  // history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(), //hash 模式
   routes: [
     {
       path: '/',
+      name: 'home',
       component: HomeView,
-      children:[
-        {
-          path:'',
-          name: 'home',
-          component:()=>import('../components/HelloWorld.vue')
-        }
-      ]
+      // children:[
+      //   {
+      //     path:'',
+      //     name: 'home',
+      //     component:()=>import('../components/HelloWorld.vue')
+      //   }
+      // ]
     }, 
     
   ]
--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,18 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
+  },
+  base:"./",
+  build:{
+    // minify:false,
+    rollupOptions:{
+      output:{
+        manualChunks:(id)=>{
+          if (id.includes("node_modules")) {
+            return "modules";
+          }
+        }
+      }
+    }
   }
 })
